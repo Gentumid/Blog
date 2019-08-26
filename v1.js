7742e274b6b954a6eefc523b720d43f32a0b5768
Gentumid <54474311+Gentumid@users.noreply.github.com>
2019-08-26 07:20:12 +0700

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,27 @@
+let a = document.createElement("script");
+a.type = "text/javascript";
+a.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+a.async = true;
+a.onload = function () {
+    textrim();
+    $(document).scroll(function () {
+        return $(document).scrollTop() > 300 ? $(".ignielToTop").addClass("show") : $(".ignielToTop").removeClass("show")
+    }), $(".ignielToTop").click(function () {
+        return $("html,body").animate({
+            scrollTop: "0"
+        })
+    })
+};
+document.getElementsByTagName("head")[0].appendChild(a);
+let footer = document.querySelector("footer .inner .right"),
+    cr = "Tema Blogger oleh <a href=\"https://www.igniel.com\" rel=\"noopener,nofollow\" target=\"_blank\" title=\"Igniel\">Igniel</a>";
+if (!footer) {
+    let a = document.createElement("div");
+    a.className = "right";
+    a.innerHTML = "Tema Blogger oleh <a href=\"https://www.igniel.com\" rel=\"noopener,nofollow\" target=\"_blank\" title=\"Igniel\">Igniel</a>";
+    document.querySelector("footer .inner").appendChild(a)
+} else {
+    if (!footer.innerHTML.includes(cr)) {
+        footer.innerHTML = "Tema Blogger oleh <a href=\"https://www.igniel.com\" rel=\"noopener,nofollow\" target=\"_blank\" title=\"Igniel\">Igniel</a>"
+    }
+}
