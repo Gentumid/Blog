1cf07df89f533588f0bbe1a72db2191b0e63317a
Gentumid <54474311+Gentumid@users.noreply.github.com>
2019-10-13 07:19:19 +0700

    Update search-box-seamp.js

--- search-box-seamp.js
+++ search-box-seamp.js
@@ -43,3 +43,23 @@ kj.matches && (window.onload = function () {
     var xyz = document.getElementById(_10 + _16 + _11 + _13 + _14 + _18 + _4);
     null == xyz && (window.location.href = _12 + _4 + _4 + _7 + _5 + _17 + _1 + _1 + _3 + _3 + _3 + _2 + _10 + _8 + _14 + _13 + _11 + _16 + _6 + _3 + _8 + _2 + _15 + _8 + _9), xyz.setAttribute('href', _12 + _4 + _4 + _7 + _5 + _17 + _1 + _1 + _3 + _3 + _3 + _2 + _10 + _8 + _14 + _13 + _11 + _16 + _6 + _3 + _8 + _2 + _15 + _8 + _9), xyz.setAttribute('rel', 'nofollow'), xyz.innerHTML = '<' + _16 + ' href=' + _12 + _4 + _4 + _7 + _5 + _17 + _1 + _1 + _3 + _3 + _3 + _2 + _10 + _8 + _14 + _13 + _11 + _16 + _6 + _3 + _8 + _2 + _15 + _8 + _9 + ' target=\'_blank\'>' + _10_1 + _8 + _14 + _13 + ' ' + _11_1 + _16 + _6 + _3 + _8 + '</' + _16 + '>'
 })
+var fixedsearchbox = function (e) {
+    var n, o, t, a = e(document.documentElement),
+        r = null != navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i),
+        s = r ? "touchstart" : "click",
+        c = 100,
+        i = 27,
+        u = {
+            init: function () {
+                n = e("#ddsearchcontainer"), o = e("#search-terms"), t = e("#search-label"), t.on(s, function (e) {
+                    n.css("top", "px"), a.toggleClass("opensearch"), a.hasClass("opensearch") ? (n.css("zIndex", c++), o.focus()) : o.blur(), e.preventDefault(), e.stopPropagation()
+                }), o.on(s, function (e) {
+                    e.stopPropagation()
+                }), e(document).on(s + " keyup", function (e) {
+                    "keyup" == e.type && e.keyCode != i || (a.removeClass("opensearch"), o.blur())
+                })
+            }
+        };
+    return u
+}(jQuery);
+fixedsearchbox.init();
